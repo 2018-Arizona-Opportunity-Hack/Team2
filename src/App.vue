a673a8ccb6898df9a11ff9402effedcd6b34360a
baehaeli <haeleeeeleeah@gmail.com>
2018-10-20 17:03:47 -0700

    pie chart

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Piechart/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Piechart from './components/Piechart.vue'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Piechart
   }
 }
 </script>
---
+++ src/components/Piechart.vue
@@ -0,0 +1,30 @@
+<template>
+   <div>
+     <apexcharts width="500" type="bar" :options="chartOptions" :series="series"></apexcharts>
+   </div>
+</template>
+<script>
+import VueApexCharts from 'vue-apexcharts'
+
+export default {
+    components: {
+      apexcharts: VueApexCharts,
+    },
+    data: function() {
+      return {
+        chartOptions: {
+          chart: {
+            id: 'vuechart-example'
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+          }
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 45, 50, 49, 60, 70, 91]
+        }]
+      }
+    },
+};
+</script>
