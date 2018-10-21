e8d87013fcc9ba93016825bc865f52a3b2f794d9
Atul Merchia <atulmerchia@gmail.com>
2018-10-21 12:25:48 -0700

    final backend

--- backend/public/index.js
+++ backend/public/index.js
@@ -1,7 +1,98 @@
+function handleLogin(){
+  firebase.auth().signInWithEmailAndPassword(
+    document.getElementById('mail').value,
+    document.getElementById('pass').value
+  )
+  .catch(err => {
+    alert(err);
+  })
+  .then(function(){
+
+  });
+}
+// exports.csvJsonReport = functions.https.onRequest((request, response)=> {
+//   var report = {'a': 0, 'b', 1};
+// })
+// function handleCreate(){
+//   createCompany();
+// }
+
 document.addEventListener('DOMContentLoaded', event => {
+  // let button = document.getElementById('login');
+  // if(button != null) button.onclick = handleLogin;
+  // else document.getElementById('create').onclick = handleCreate;
 
   document.getElementById('company').onclick = createCompany;
   document.getElementById('employee').onclick = createEmployee;
+
+  document.getElementById('login').onclick = function(){
+    let username = firebase.auth().currentUser.uid, numEntries;
+    let csvContent = '';
+
+    promise = firebase.database().ref('/users/' + username + '/compId')
+      .once('value', data => {
+        compId = data.val();
+      }
+      , err => {
+        console.log(err);
+      });
+
+    let promise2 = promise.then(function(value){
+        console.log(promise);
+        firebase.database().ref('/comps/' + compId).orderByChild('transactionId').endAt(0).on('child_added', snapshot => {
+          for(element in snapshot.val())
+            csvContent += element + ',';
+          csvContent += '\r\n';
+        });
+      }, function(){});
+
+    let promise3 = promise2.then(function(value){
+        console.log(promise2);
+        firebase.database().ref('/comps/' + compId).orderByChild('transactionId').on('child_added', snapshot => {
+          snap = snapshot.val();
+          if(snap['compId'] == undefined){
+            for(element in snap)
+              csvContent += snap[element] + ',';
+            csvContent += '\r\n';
+          }
+          console.log(csvContent);
+        })
+      }, function(){});
+
+    let promise4 = promise3.then(function(value){
+      var link = document.createElement("a");
+      link.download = 'transactions.csv';
+      link.href = 'data:text/csv;charset=utf-8' + csvContent;
+      link.click();
+    }, function(){});
+  };
+
+  function piechartcreator(){
+    var map = {};
+    var compId;
+    let username = firebase.auth().currentUser.uid;
+    firebase.database().ref('/users/' + username + '/compId')
+      .once('value', data => {
+        compId = data.val();
+      }
+      , err => {
+        console.log(err);
+      })
+      .then(function(){
+        firebase.database().ref('/comps/' + compId).orderByChild('data').on('child_added', snapshot => {
+          snap = snapshot.val();
+          if(snap['date'] != null){
+            if(snap['date'].substring(0,2) == 10 && snap['date'].substring(6) == 2018)
+              if(map[snap['category']] == undefined)
+                map[snap['category']] = 0;
+              map[snap['category']] += parseInt(snap['amount']);
+          }
+        });
+      })
+      .then(function(){
+        console.log(map);
+      });
+  }
 
   document.getElementById('submit').onclick = function(){
 
@@ -73,7 +164,8 @@ function createCompany(){
 
   firebase.auth().createUserWithEmailAndPassword(mail, pass)
     .catch(err => {
-      console.log(err);
+      alert(err);
+      return;
     })
     .then(function(){
       firebase.auth().currentUser.updateProfile({
